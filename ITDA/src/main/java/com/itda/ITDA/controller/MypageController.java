105c713b285afafab95c40eda40feadbbc2bb516
blue <blue@DESKTOP-59OT208>
2023-10-02 10:44:38 +0900

    마이페이지 css 배경 수정

--- ITDA/src/main/java/com/itda/ITDA/controller/MypageController.java
+++ ITDA/src/main/java/com/itda/ITDA/controller/MypageController.java
@@ -17,41 +17,50 @@ public class MypageController {
 		
 	}
 	
+	// 마이페이지 메인(구독채널의 채널)
 	@GetMapping(value="/subscriptions")
 	public String subsctiptions() {
 		return "mypage/subscriptions";
 	}
 	
 	/*
-	 * @GetMapping(value = "/channelmain") public String Detail() { return
-	 * ("channel/ChannelMain"); }
+	 * @GetMapping(value = "/channelmain") 
+	 * public String Detail() { 
+	 * return ("channel/ChannelMain"); 
+	 * }
 	 */
 	
+	// 마이페이지 구독채널의 최신 콘텐츠
 	@GetMapping(value="/subscriptions/contents")
 	public String subscriptions_contents() {
 		return"mypage/subscriptions_contents";
 	}
 	
+	// 마이페이지 쿠폰
 	@GetMapping(value="/coupons")
 	public String coupons() {
 		return"mypage/coupons";
 	}
 	
+	// 마이페이지 설정(구독 설정)
 	@GetMapping(value="/notification")
 	public String notification() {
 		return "mypage/notification";
 	}
 	
+	// 마이페이지 결제 내역
 	@GetMapping(value="/payment/subscriptions")
 	public String payment_subscriptions() {
 		return "mypage/payment_subscriptions";
 	}
 	
+	// 마이페이지 결제 내역 자세히 보기
 	@GetMapping(value="/payment/subscriptions/episode")
 	public String payment_subscriptions_episode() {
 		return "mypage/payment_subscriptions_episode";
 	}
 	
+	// 관심 콘텐츠(좋아요 콘텐츠)
 	@GetMapping(value="/contents")
 	public String contents() {
 		return "mypage/contents";
