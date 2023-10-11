01d934212720089d50453fa4aeb4be6c300de849
pphj <138371414+pphj@users.noreply.github.com>
2023-10-12 08:58:52 +0900

    1012 9시 commit

--- ITDA/src/main/resources/static/js/admin/problem.js
+++ ITDA/src/main/resources/static/js/admin/problem.js
@@ -18,10 +18,8 @@
 		if (href == "") {					//href가 빈 문자열일 경우
 			if (isNaN(digit)){				//digit이 숫자가 아닌 경우
 				gray = "gray";				//href가 빈 문자열일 경우 + digit이 숫자가 아닌 경우 = 버튼색을 회색으로 바꾼다
-			
 			}else {
 				active = "active";			//href가 빈 문자열일 경우 + digit이 숫자인경우 = 버튼에 active를 준다
-				
 			}
 		}//if end
 		
@@ -34,62 +32,66 @@
 		return output;
 	}
 	
-	
+
+		
 	
 	function ajax(sdata) {
 		console.log(sdata)
-		let token = $("meta[name='_csrf']").attr("content");	
-		let header = $("meta[name='_csrf_header']").attr("content");
+		//let token = $("meta[name='_csrf']").attr("content");	
+		//let header = $("meta[name='_csrf_header']").attr("content");
 		$.ajax({
 			type : "post",
 			data : sdata,
-			url	 : "FAQList_ajax",
+			url	 : "problemList_ajax",
 			dataType : "json",
 			cache : false,
-			beforeSend : function(xhr) {
-				xhr.setRequestHeader(header, token);
-			},
+			//beforeSend : function(xhr) {
+			//	xhr.setRequestHeader(header, token);
+			//},
 			success : function(data) {
 				$("#viewcount").val(data.limit);
-				$("thead").find("span").text("글 개수 : " + data.listcount);
+				$("thead").find("span").text("총 신고 수 : " + data.sumListCount);
 				
 				if (data.listcount > 0) {
 					$("tbody").remove();
 					let num = data.listcount - (data.page - 1) * data.limit;
-					console.log(num)
 					let output = "<tbody>";
-					$(data.boardlist).each(
+					$(data.problemList).each(
 						function(index, item) {
-							output += '<tr><td>' + (num--) + '</td>'
-							const blank_count = item.board_re_lev * 2 + 1;
-							let blank = '&nbsp;';
+							output += '<tr><td class="text-center">' + (num--) + '</td>'
 							
-							for (let i = 0; i < blank_count; i++){
-								blank += '&nbsp;&nbsp;';
-							
+							let status = "";
+							switch (item.statusid) {
+								case 1:
+									status = "정상";
+									break;
+								case 2:
+									status = "일시 정지";
+									break;
+								case 3:
+									status = "정지";
+									break;
+								default:
+									status = "출력 오류";
 							}
 							
-							let img = "";
-							if (item.board_re_lev > 0) {
-								img = "<img src='../image/line.gif'>";
-								
-							}
-							
-							let subject = item.board_subject;
-							if (subject.length >= 20) {
-								subject = subject.substr(0, 20) + "...";		//0부터 20개 부분 문자열 추출
-							
-							}
-							
-							output += "<td><div>" + blank + img
-								    + ' <a href="detail?num=' + item.board_num + '">'
-								    + subject.replace(/</g,'&lt;').replace(/>/g,'&gt;')
-								    + '</a>[' + item.cnt + ']</div></td>'
-								    + '<td><div>' + item.board_name + '</div></td>'
-									+ '<td><div>' + item.board_date + '</div></td>'
-									+ '<td><div>' + item.board_readcount + '</div></td></tr>'
-							
-							
+							output += '<td class="text-center targetSickId"><div>' + item.sickId + '</div></td>'
+									+ '<td class="text-center"><div>' + item.sumCount + '</div></td>'
+									+ '<td class="text-center"><div>' + status + '</div></td>'
+									+ '<td class="td-actions text-center">'
+							        + '<button type="button" rel="tooltip"'
+									+ ' class="btn btn-danger btn-icon btn-sm Pause" data-original-title="" title="">'
+							        + ' 	<i class="ni ni-circle-08 pt-1"></i>&nbsp;&nbsp;&nbsp;일시정지'
+							        + '</button>&nbsp;'
+							        + '<button type="button" rel="tooltip"'
+									+ ' class="btn btn-danger btn-icon btn-sm Stop" data-original-title="" title="">'
+							        + ' 	<i class="ni ni-circle-08 pt-1"></i>&nbsp;&nbsp;&nbsp;정지'
+							        + '</button>&nbsp;'
+							        + '<button type="button" rel="tooltip"'
+									+ ' class="btn btn-info btn-icon btn-sm Clear" data-original-title="" title="">'
+							        + ' 	<i class="ni ni-circle-08 pt-1"></i>&nbsp;&nbsp;&nbsp;해제'
+							        + '</button>'
+							        + '</td></tr>'
 					})//each end
 					output += "</tbody>"
 					$('table').append(output);			//table 완성
@@ -100,7 +102,7 @@
 					let digit = '이전&nbsp;'
 					let href = "";
 					
-					if (data.page > 1) {				//이전 버튼의 링크 설정
+					if (data.page > 1) {											//이전 버튼의 링크 설정
 						href = 'href=javascript:go(' + (data.page - 1) + ')';		//go function 실행
 					}			//현재 페이지가 3인 상태에서 이전 버튼을 누르면 href=javascript:go(2)가 되는 것
 					output += setPaging(href, digit);
@@ -109,7 +111,7 @@
 						digit = i;
 						href = "";
 						
-						if (i != data.page) {			//현재 페이지가 아닌 페이지 번호들
+						if (i != data.page) {										//현재 페이지가 아닌 페이지 번호들
 							href = 'href=javascript:go(' + i + ')';					//go function 실행
 						}
 						output += setPaging(href, digit);
@@ -119,14 +121,14 @@
 					digit = '&nbsp;다음&nbsp;';
 					href = "";
 					
-					if (data.page < data.maxpage) {		//다음 버튼의 링크 설정
+					if (data.page < data.maxpage) {									//다음 버튼의 링크 설정
 						href = 'href=javascript:go(' + (data.page + 1) + ')';		//go function 실행
 					}
 					output += setPaging(href, digit);
 					
 					$('.pagination').append(output);
 						 
-				}//data.listcount end
+				}
 				
 			},//success end
 			
@@ -138,18 +140,50 @@
 		
 	}//ready end
 	
-	
+	$(document).on("click", ".Pause", function() {
+	    // 현재 클릭한 버튼이 속한 행에서 필요한 데이터를 가져옴
+	    let targetSickId = $(this).closest("tr").find(".targetSickId").text();
+    	$(".selectUserId").val(targetSickId);
+    	$("#problemPauseForm input[name='approve']").val("2");
+    		
+    	let result = confirm("회원상태를 일시정지 하겠습니까?");
+		    
+		if (result) {
+			$("form[name='problemPauseForm']").submit();
+		}
+	});
+
+	$(document).on("click", ".Stop", function() {
+	    let targetSickId = $(this).closest("tr").find(".targetSickId").text();
+    	$(".selectUserId").val(targetSickId);
+    	$("#problemStopForm input[name='approve']").val("3");
+    		
+    	let result = confirm("회원상태를 정지 하겠습니까?");
+		    
+		if (result) {
+			$("form[name='problemStopForm']").submit();
+		}
+	});
+
+	$(document).on("click", ".Clear", function() {
+	    let targetSickId = $(this).closest("tr").find(".targetSickId").text();
+    	$(".selectUserId").val(targetSickId);
+    	$("#problemClearForm input[name='approve']").val("1");
+    		
+    	let result = confirm("상태를 되돌리겠습니까?");
+		    
+		if (result) {
+			$("form[name='problemClearForm']").submit();
+		}
+	});
 	
 	$(function(){
-		
-		$("button").click(function(){
-			location.href="write";				//버튼 클릭시 write로 이동
-		
-		})// click end
-		
 		$("#viewcount").change(function(){
-			go(1); 								// 보여줄 페이지를 1페이지로 설정한다
+			go(1); 									// 보여줄 페이지를 1페이지로 설정한다
 		
 		})//change end
 		
+		
 	});//ready end
+
+	
